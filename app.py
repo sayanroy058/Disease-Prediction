4304c951b93e8f10639d2da3d02487d12f3718fb
Sayan Roy <55015854+sayanroy058@users.noreply.github.com>
2023-11-11 14:48:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-import streamlit as st
+from flask import Flask, request, render_template
 import pandas as pd
-import sklearn
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
+app = Flask(__name__)
 
 # Assuming df is your training dataframe and df_test is your test dataframe
 def train_model():
@@ -25,27 +26,19 @@ def train_model():
     # Optionally calculate the accuracy on the test set
     predictions = model.predict(X_test)
     accuracy = accuracy_score(y_test, predictions)
-    st.write(f"Model trained with accuracy: {accuracy:.2f}")
+    print(f"Model trained with accuracy: {accuracy:.2f}")
 
     return model
 
 # Train model on startup
 model = train_model()
 
-# Streamlit app
-st.title("Medical Diagnosis App")
-
-# Collect symptoms from the user
-symptoms = {}
-for i in range(1, 134):  # Assuming you have 133 symptoms
-    symptoms[f'Symptom {i}'] = st.checkbox(f'Symptom {i}')
-
-if st.button("Predict"):
-    # Prepare symptoms for the model
-    input_symptoms = list(map(int, list(symptoms.values())))
-    
-    # Make a prediction
-    prediction = model.predict([input_symptoms])[0]
-
-    # Display the result
-    st.write(f"The predicted prognosis is: {prediction}")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Get symptoms from form and prepare them for the model
+        symptoms = request.form.to_dict()
+        symptoms = list(map(int, symptoms.values()))  # Example preprocessing
+        prediction = model.predict([symptoms])
+        return render_template('result.html', prediction=prediction[0])
+    return render_template('index.html')
